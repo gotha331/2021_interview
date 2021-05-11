e2abbc9940d88369c5e1e111f12b49fb3159deeb
liujiang <liujiang1214@ichinae.com>
2021-05-11 18:18:05 +0800

    init

---
+++ 01-MVVM响应式原理/MVue.js
@@ -0,0 +1,15 @@
+/*
+ * @Files: 
+ * @Version: 1.0
+ * @Author: jiang.liu
+ * @Date: 2021-05-11 18:15:10
+ * @LastEditors: jiang.liu
+ * @LastEditTime: 2021-05-11 18:16:43
+ */
+class MVue{
+  constructor(options) {
+    this.$el = options.el;
+    this.$data = options.data;
+    this.$options = options;
+  }
+}
