813e84478b2a99d90623c229c48cdc96cc6d5445
liujiang <liujiang1214@ichinae.com>
2021-06-08 10:54:42 +0800

    feat: componsition api

--- hello-vite/src/components/HelloWorld.vue
+++ hello-vite/src/components/HelloWorld.vue
@@ -4,7 +4,7 @@
  * @Author: jiang.liu
  * @Date: 2021-06-07 21:24:14
  * @LastEditors: jiang.liu
- * @LastEditTime: 2021-06-07 22:47:43
+ * @LastEditTime: 2021-06-08 10:53:11
 -->
 <template>
   <h1>{{ msg }}</h1>
@@ -17,9 +17,11 @@
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
   </p>
 
-  <button @click="data.counter++">counter is: {{ data.counter }}</button>
-  <p>{{ data.counter }}</p>
-  <p>{{ data.doubleCounter }}</p>
+  <button @click="counter++">counter is: {{ counter }}</button>
+  <p>{{ counter }}</p>
+  <p>{{ doubleCounter }}</p>
+  <p>{{ msg2 }}</p>
+  <p ref="desc"></p>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
@@ -27,7 +29,16 @@
 </template>
 
 <script>
-import { defineProps, reactive, computed, onMounted, onUnmounted } from "vue";
+import {
+  defineProps,
+  reactive,
+  computed,
+  onMounted,
+  onUnmounted,
+  ref,
+  toRefs,
+  watch,
+} from "vue";
 
 export default {
   name: "HelloWorld",
@@ -35,26 +46,46 @@ export default {
     msg: String,
   },
   setup() {
-    const data = reactive({
-      counter: 1,
-      doubleCounter: computed(() => data.counter * 2),
+    const { counter, doubleCounter } = useCounter();
+
+    // 其他数据，ref单值响应式
+    const msg2 = ref("some message");
+
+    // 使用元素引用
+    const desc = ref(null);
+
+    watch(counter, (val, oldVal) => {
+      console.log(val);
+      console.log(oldVal);
+
+      const p = desc.value;
+      p.textContent = `counter change from ${oldVal} to ${val}`;
     });
 
-    let timer;
-
-    onMounted(() => {
-      timer = setInterval(() => {
-        data.counter++;
-      }, 1000);
-    });
-
-    onUnmounted(() => {
-      clearInterval(timer);
-    });
-
-    return { data };
+    return { counter, doubleCounter, msg2, desc };
   },
 };
+
+function useCounter() {
+  const data = reactive({
+    counter: 1,
+    doubleCounter: computed(() => data.counter * 2),
+  });
+
+  let timer;
+
+  onMounted(() => {
+    timer = setInterval(() => {
+      data.counter++;
+    }, 1000);
+  });
+
+  onUnmounted(() => {
+    clearInterval(timer);
+  });
+
+  return toRefs(data);
+}
 </script>
 
 <style scoped>
